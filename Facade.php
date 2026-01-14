f0916af0d18cbc885f692d9158b6bb8aabec6585
wpint <ali.barzegar.rahimi@gmail.com>
2026-01-14 17:08:59 +0330

    - Remove useless Facades - Remove useless classes - Extends Route Facade based on the extended router

--- Facade.php
+++ Facade.php
@@ -1,35 +1,18 @@
 <?php 
 namespace Wpint\Support;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Facade as FacadesFacade;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\View;
+use Wpint\Support\Facades\AdminRoute;
+use Wpint\Support\Facades\AjaxRoute;
+use Wpint\Support\Facades\JWT;
+use Wpint\Support\Facades\RestRoute;
+use Wpint\Support\Facades\WebRoute;
+use Wpint\Support\Facades\WPAPI;
+use Wpint\Support\Facades\WPFile;
+use Wpint\Support\Facades\WPFileDirect;
 
 class Facade extends FacadesFacade
 {
-
-
-    /**
-     * Resolve the facade root instance from the container.
-     *
-     * @param  string  $name
-     * @return mixed
-     */
-    protected static function resolveFacadeInstance($name)
-    {
-        return static::$app->make($name);
-    }
-
 
     /**
      * Get the application default aliases.
@@ -38,21 +21,11 @@ class Facade extends FacadesFacade
      */
     public static function defaultAliases()
     {
-        return collect([
-            'Arr' => Arr::class,
-            'Blade' => Blade::class,
-            'Bus' => Bus::class,
-            'Cache' => Cache::class,
-            'Config' => Config::class,
-            'Cookie' => Cookie::class,
-            'DB' => DB::class,
-            'Eloquent' => Model::class,
-            'Event' => Event::class,
-            'File' => File::class,
-            'Str' => Str::class,
-            'View' => View::class,
+        return parent::defaultAliases()->merge([
+            'wpapi'             =>  WPAPI::class,
+            'wp.file.base'      =>  WPFile::class,
+            'wp.file.direct'    =>  WPFileDirect::class,
         ]);
     }
-
 
 }
